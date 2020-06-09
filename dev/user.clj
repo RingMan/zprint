b5573a0479ce03fb3a7dbaa0b6a0b60ab432db59
David Kettering <david.ringman@gmail.com>
2020-06-09 17:26:29 -0500

    Personalize zprint REPL experience (dev only)
    Customizes zprint color-map, uneval color map, and a few map
    options.

--- dev/user.clj
+++ dev/user.clj
@@ -20,6 +20,7 @@
                      zprint-fn zprint-fn-str
                      czprint czprint-str czprint-fn czprint-fn-str
                      configure-all! set-options!]]
+            [zprint.cutil :refer :all]
             [zprint.ansi :refer [ansi-codes color-str]]))
 
 (alias 'c 'clojure.core)
@@ -42,4 +43,45 @@
           (var-set render-fn)))
 
 (update-print-fn! render)
+
+(def zp-colors
+  {:brace :white,
+   :bracket :white,
+   :char :bright-cyan,
+   :comma :bright-white,
+   :comment :bright-black,
+   :deref :red,
+   :false :bright-magenta,
+   :fn :bright-red,
+   :hash-brace :white ,
+   :hash-paren :white,
+   :keyword :bright-blue,
+   :nil :bright-magenta,
+   :none :white,
+   :number :bright-magenta,
+   :paren :white,
+   :quote :bright-yellow,
+   :regex :bright-cyan,
+   :string :bright-green,
+   :symbol :bright-white,
+   :syntax-quote :bright-yellow,
+   :syntax-quote-paren :white,
+   :true :bright-magenta,
+   :uneval :bright-red,
+   :unquote :bright-yellow,
+   :unquote-splicing :bright-yellow,
+   :user-fn :bright-yellow})
+
+(def zp-opt
+  {:color-map zp-colors
+   :map {:comma? true
+         :indent 2
+         :key-order [:tag :id :name]
+         :key-color (kcol (ks->v [:tag :id :name] [:bold]) zp-colors)}
+   :vector {:wrap-after-multi? false}
+   :uneval {:color-map zp-colors}})
+
+(defn init-opt [] (set-options! zp-opt))
+
+(set-options! zp-opt)
 
