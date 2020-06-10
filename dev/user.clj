85cebe81c768756896023337283389dda9893097
David Kettering <david.ringman@gmail.com>
2020-06-09 22:57:07 -0500

    Add fns to install czprint as REPL print function

--- dev/user.clj
+++ dev/user.clj
@@ -26,3 +26,20 @@
 
 (integrant.repl/set-prep! (constantly {}))
 
+; next two fns lifted from whidbey and adapted to czprint
+
+(defn render
+  "Renders the given value for display by pretty-printing it on the given writer
+  using czprint."
+  [value writer]
+  (binding [*out* writer] (czprint value)))
+
+(defn update-print-fn!
+  "Updates nREPL's printing configuration to use `render-fn` nREPL 0.6.0+ only."
+  [render-fn]
+  (some-> (find-ns 'nrepl.middleware.print)
+          (ns-resolve '*print-fn*)
+          (var-set render-fn)))
+
+(update-print-fn! render)
+
